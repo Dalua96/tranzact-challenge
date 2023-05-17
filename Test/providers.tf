e955d2eb4a8c8e29bc97c4d0e0e7b5464007ffbd
Antonio Alvarado <Antonio.Alvarado@kyndryl.com>
2023-05-17 00:07:17 -0500

    DEL: tf files states

--- Test/providers.tf
+++ Test/providers.tf
@@ -13,8 +13,6 @@ provider "azurerm" {
   features {
 
   }
-  #client_id                  = "e21c23c1-b1fe-41e8-aa58-57f84baf5fb8"
-  #client_secret              = "-x~8Q~Yy5332F9pSHwtJ3BDpUa8rnq4cD0cLAcmC"
   tenant_id                  = "a618cda7-d591-41f5-8ff9-6ddea7e31dc2"
   subscription_id            = "602d5212-3cb9-4118-b810-0710e20d98a3"
   skip_provider_registration = true
