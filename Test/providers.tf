38255f94f5fd9a0b8eb6633847a6140b11be4c0b
Antonio Alvarado <Antonio.Alvarado@kyndryl.com>
2023-05-17 17:40:52 -0500

    ADD: detec ip public githubactions

--- Test/providers.tf
+++ Test/providers.tf
@@ -5,23 +5,19 @@ terraform {
       version = "=3.56.0"
     }
   }
-  #backend "azurerm" {
-  #   resource_group_name  = "challenge-test-rg"
-  #   storage_account_name = "challengetestsa"
-  #   container_name       = "challenge-test-container"
-  #   key                  = "terraform.tfstate"
-  #}
+  backend "azurerm" {
+    resource_group_name  = "challenge-test-rg"
+    storage_account_name = "challengetestsa"
+    container_name       = "challenge-test-container"
+    key                  = "terraform.tfstate"
+  }
 
 }
-
-
 
 provider "azurerm" {
   features {
 
   }
-  tenant_id                  = "a618cda7-d591-41f5-8ff9-6ddea7e31dc2"
-  subscription_id            = "602d5212-3cb9-4118-b810-0710e20d98a3"
   skip_provider_registration = true
 }
 
